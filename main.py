1dad6e19b9506a4c1e57ab548f58b5b2b0d31f1d
Chris Strutton <chris@rodandfly.ca>
2022-11-18 11:20:27 -0500

    connect to PLC and test

--- main.py
+++ main.py
@@ -2,27 +2,33 @@
 import time
 from pylogix import PLC
 
-CHECK_TAG = ''
-CODE_TAG = ''
-GOOD_TAG = ''
-BAD_TAG = ''
+CHECK_TAG = 'Verify_Barcode'
+CODE_TAG = 'Laser_QR_Code_Text'
+GOOD_TAG = 'Barcode_OK'
+BAD_TAG = 'Barcode_Not_OK'
+LASER_JOB = 'Laser_Job_Req_num'
 
 def startup():
     pass
 
 
 def check_barcode(barcode):
+    print(barcode)
     return True
 
 
 if __name__ == "__main__":
+    startup()
+
     with PLC() as comm:
         comm.IPAddress = '192.168.1.1'
         read = True
         while read:
             try:
                 if comm.Read(CHECK_TAG):
-                    tag = comm.Read(CODE_TAG)
+                    tags = comm.Read([CODE_TAG, LASER_JOB])
+                    mark = tags[0].Value
+                    job = tags[1].Value
                     status =  check_barcode(CODE_TAG)
                     comm.Write([(GOOD_TAG, status), (BAD_TAG, not status)])
                 else:
